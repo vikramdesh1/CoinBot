913267ae161f68377b28edfbb668571432632cff
Vikram Deshmukh <vikramdesh1@live.com>
2017-07-11 16:46:15 -0400

    experimenting with timers and http

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const http = require('http');
+/*const http = require('http');
 
 http.createServer((request, response) => {
 
-request.on('end', () => {
-console.log("Here!");
+request.on('error', (err) => {
+console.log("Error!");
+}).on('data', (chunk) => {
+console.log("Chunk!");
+}).on('end', () => {
+console.log("End!");
+response.statusCode = 200;
 response.end("Hello World!");
 });
 
-}).listen(8080);
+}).listen(8080);*/
+
+function intervalTest() { 
+console.log("Boop!");
+}
+
+setInterval(intervalTest, 1000);
