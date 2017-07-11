a7006182cb01dff370b77ab062295d22e9ddcfe8
Vikram Deshmukh <vikramdesh1@live.com>
2017-07-11 17:05:57 -0400

    testing coinbase api

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-/*const http = require('http');
+var Client = require('coinbase').Client;
+var coinbaseClient = new Client({'apiKey':'lxESKgeyFx5sOaGD', 'apiSecret':'VLwZ3tju0mJZovCueZvfQT1fZO5rUJa7'});
+const http = require('http');
 
-http.createServer((request, response) => {
+/*http.createServer((request, response) => {
 
-request.on('error', (err) => {
-console.log("Error!");
-}).on('data', (chunk) => {
-console.log("Chunk!");
-}).on('end', () => {
-console.log("End!");
-response.statusCode = 200;
-response.end("Hello World!");
-});
+    request.on('error', (err) => {
+        console.log("Error!");
+    }).on('data', (chunk) => {
+        console.log("Chunk!");
+    }).on('end', () => {
+        console.log("End!");
+        response.statusCode = 200;
+        response.end("Hello World!");
+    });
 
 }).listen(8080);*/
 
 function intervalTest() { 
-console.log("Boop!");
+    console.log("Boop!");
 }
 
-setInterval(intervalTest, 1000);
+function testCoinbase() {
+    coinbaseClient.getAccounts({}, function(err, accounts) {
+        accounts.forEach(function(acct) {
+            console.log(acct.name + " -- " + acct.balance.amount);
+        });
+    });
+}
+
+//setInterval(intervalTest, 1000);
+
+testCoinbase();
