642580429353f62ff5a56a86def3495cb050ffab
Vikram Deshmukh <vikramdesh1@live.com>
2017-07-12 22:18:43 -0400

    finished module for selling all eth over a minimum profit margin

--- index.js
+++ index.js
@@ -1,33 +1,95 @@
 var Client = require('coinbase').Client;
-var coinbaseClient = new Client({'apiKey':'lxESKgeyFx5sOaGD', 'apiSecret':'VLwZ3tju0mJZovCueZvfQT1fZO5rUJa7'});
-const http = require('http');
+var coinbaseClient = new Client({ 'apiKey': 'lxESKgeyFx5sOaGD', 'apiSecret': 'VLwZ3tju0mJZovCueZvfQT1fZO5rUJa7' });
 
-/*http.createServer((request, response) => {
-
-    request.on('error', (err) => {
-        console.log("Error!");
-    }).on('data', (chunk) => {
-        console.log("Chunk!");
-    }).on('end', () => {
-        console.log("End!");
-        response.statusCode = 200;
-        response.end("Hello World!");
-    });
-
-}).listen(8080);*/
-
-function intervalTest() { 
-    console.log("Boop!");
-}
+const minimumETHProfitMargin = 0.05;
 
 function testCoinbase() {
-    coinbaseClient.getAccounts({}, function(err, accounts) {
-        accounts.forEach(function(acct) {
-            console.log(acct.name + " -- " + acct.balance.amount);
+
+    checkETHProfitMargin();
+
+    /* coinbaseClient.getPaymentMethods({}, function(err, pms) {
+        console.log(pms);
+    }); */
+
+    /* coinbaseClient.getAccounts({}, function(err, accounts) {
+        console.log(accounts);
+    }); */
+
+}
+
+function checkETHProfitMargin() { //Check profit margins and sell all if margin is higher then limit
+    console.log(new Date().toString());
+    coinbaseClient.getAccount("28b44d24-8162-5983-895f-08a5079bc86c", function (err, account) { //ETH Wallet
+        account.getTransactions({}, function (err, txs) {
+            if (err) {
+                console.error(err);
+                throw "Error in checkETHProfitMargin - account.getTransactions";
+            }
+            var lastBuy;
+            for (var i = 0; i < txs.length; i++) {
+                if (txs[i].type.toLowerCase() == "buy" && txs[i].status.toLowerCase() == "completed") {
+                    lastBuy = txs[i];
+                    break;
+                }
+            }
+            if (lastBuy) {
+                var sellParams = {
+                    "amount": account.balance.amount,
+                    "currency": "ETH",
+                    "payment_method": "36dfb503-14ee-522f-86fc-ea62f705e4c8", //USD Wallet       
+                    "quote": true
+                };
+                account.sell(sellParams, function (err, tx) {
+                    if (err) {
+                        console.error(err);
+                        throw "Error in checkETHProfitMargin - account.sell (quote)";
+                    }
+                    var lastBuyPrice = lastBuy.native_amount.amount;
+                    var currentSellPrice = tx.total.amount;
+                    var currentProfitMargin = currentSellPrice / lastBuyPrice;
+                    console.log("Last buy price - " + lastBuyPrice);
+                    console.log("Current sell price - " + currentSellPrice);
+                    console.log("Profit margin - " + parseFloat(((currentProfitMargin) - 1) * 100).toFixed(2) + "%");
+                    if ((currentProfitMargin - 1) > minimumETHProfitMargin) {
+                        var sellParams = {
+                            "amount": account.balance.amount,
+                            "currency": "ETH",
+                            "payment_method": "36dfb503-14ee-522f-86fc-ea62f705e4c8"
+                        }; //USD Wallet                
+                        account.sell(sellParams, function (err, tx) { //SELL IT ALL
+                            if (err) {
+                                console.error(err);
+                                throw "Error in checkETHProfitMargin - account.sell (sell)";
+                            }
+                            console.log("Sold all ETH for a profit margin of " + parseFloat(((currentProfitMargin) - 1) * 100).toFixed(2) + "%");
+                        });
+                    } else {
+                        console.log("Waiting to sell...");
+                    }
+                    console.log("----------");
+                });
+            }
         });
     });
 }
 
-//setInterval(intervalTest, 1000);
+function testSell() {
+    coinbaseClient.getAccount("28b44d24-8162-5983-895f-08a5079bc86c", function (err, account) { //ETH Wallet
+        var sellParams = {
+            "amount": "0.01",
+            "currency": "ETH",
+            "payment_method": "36dfb503-14ee-522f-86fc-ea62f705e4c8"
+        }; //USD Wallet                
+        account.sell(sellParams, function (err, tx) { //SELL IT ALL
+            if (err) {
+                console.error(err);
+                throw "Error in checkETHProfitMargin - account.sell (sell)";
+            }
+            console.log(tx);
+        });
+    });
+}
 
-testCoinbase();
+setInterval(checkETHProfitMargin, 10000);
+
+//testCoinbase();
